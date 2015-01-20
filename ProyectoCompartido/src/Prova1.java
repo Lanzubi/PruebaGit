cfdae253044ef73a15fd8d719a1fc1df0d023efe
alumne <alumne@localhost>
2015-01-20 20:14:45 +0100

    Prueba

---
+++ ProyectoCompartido/src/Prova1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author alumne
+ *
+ */
+public class Prova1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
