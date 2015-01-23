bc94199c94338b9bd936a045d33770f6f57dd504
alumne <alumne@localhost>
2015-01-23 16:25:31 +0100

    Jacobooo sepa usted

--- ProyectoCompartido/src/PrimeraPrueba/Prova1.java
+++ ProyectoCompartido/src/PrimeraPrueba/Prova1.java
@@ -13,7 +13,7 @@ public class Prova1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Jacoooob no sabe jugar al counteeeeer nadaaaaa :3");
+		System.out.println("Jacoooob no sabe jugar a nadaaaaa :3");
 
 	}
 
